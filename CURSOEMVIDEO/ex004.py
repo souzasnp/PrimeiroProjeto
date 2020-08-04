5e5112c2bce24ec65f6bd08aac14c0e15bbec77c
souzasnp <68926395+souzasnp@users.noreply.github.com>
2020-08-04 16:55:34 -0400

    Curso em Video - ex004
    Foi alterado a linha onde era definido o tipo primitivo do comando ler, também foi acrescentado algumas linhas de códigos.

--- CURSOEMVIDEO/ex004.py
+++ CURSOEMVIDEO/ex004.py
@@ -1,3 +1,7 @@
-n1 = float(input('ENTRE COM O PRIMEIRO VALOR: '))
+n1 = input('ENTRE COM O PRIMEIRO VALOR: ')
 print('Qual o tipo primitivo do valor ? {} O valor digitado é : {}.'
       .format(type(n1), n1))
+print('O valor digitado é Numérico ? ', n1.isnumeric())
+print('O Valor digitado é Alfabético? ', n1.isalpha())
+print('O Valor digitado tem espaçamento? ', n1.isspace())
+print('O valor digitado é Alfanumérico? ', n1.isalnum())
