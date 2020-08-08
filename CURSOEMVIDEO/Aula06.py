fc6b142720b09793c0c0bde60cc669fa4486c652
souzasnp <rodrigosouzasnp@hotmail.com>
2020-08-07 22:48:57 -0400

    CURSO EM VIDEO

--- CURSOEMVIDEO/Aula06.py
+++ CURSOEMVIDEO/Aula06.py
@@ -1,9 +1,28 @@
-print ('Exercicios da Aula 07 - curso em video')
+#print ('Exercicios da Aula 07 - curso em video')
 #a = 5 + (3*2)
 #print (a)
 
 #b = (3*5) + (4**2)
 #print (b)
-a = int(input('Insira o primeiro valor: '))
-b = int(input('Insira o segundo valor: '))
-print(' O valor da soma é : {}.\n O valor da divisão inteira é : {}.\n O resto da divisão é : {:.3f} '.format(a+b, a//b, a%b))
+#a = int(input('Insira o primeiro valor: '))
+#b = int(input('Insira o segundo valor: '))
+#print(' O valor da soma é : {}.\n O valor da divisão inteira é : {}.\n O resto da divisão é : {:.3f} '.format(a+b, a//b, a%b))
+
+#Fase 8  a 
+#Exemplo importando toda a biblioteca
+import math
+num = int(input('Digite um número: '))
+raiz = math.sqrt(num)
+print('A raiz de {} é igual a {}'.format(num, math.ceil(raiz)))
+#Exemplo importando somente a função
+from math import sqrt
+num = int(input('Digite um número: '))
+raiz = sqrt (num)
+print('A raiz de {} é igual a {}').format(num, raiz)
+
+#Exemplo de como gerar dados aleatórios
+import random
+num = random.random() ou random.randint
+print(num)
+
+import emoji
