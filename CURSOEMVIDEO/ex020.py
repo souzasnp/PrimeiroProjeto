61c426b74e01a780c7c4ea494ed01d96819fbbba
souzasnp <68926395+souzasnp@users.noreply.github.com>
2020-08-10 17:11:33 -0400

    resolução de exercicios

--- CURSOEMVIDEO/ex020.py
+++ CURSOEMVIDEO/ex020.py
@@ -1,1 +1,10 @@
 #O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatros alunos e mostre a ordem sorteada.
+import random
+aluno1 = str(input('Primeiro Aluno: '))
+aluno2 = str(input('Segundo Aluno: '))
+aluno3 = str(input('Terceiro Aluno: '))
+aluno4 = str(input('Quarto Aluno: '))
+resul = [aluno1, aluno2, aluno3, aluno4]
+random.shuffle(resul)
+print('Ordem de Apresentação:')
+print(resul)
--- CURSOEMVIDEO/ex021.py
+++ CURSOEMVIDEO/ex021.py
@@ -1,1 +1,5 @@
 #Faça um programa em python que abra e reproduza o áudio de um arquivo mp3.
+import pygame
+#print('Curta o SOM :D ')
+pygame.mixer.load('aindatoai.mp3')
+pygame.mixer_music.play()
