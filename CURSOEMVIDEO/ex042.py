ccc5dee578cf070c0a720abfe41ce04f2c26aa37
souzasnp <68926395+souzasnp@users.noreply.github.com>
2020-09-08 17:29:29 -0400

    Update ex042.py

--- CURSOEMVIDEO/ex042.py
+++ CURSOEMVIDEO/ex042.py
@@ -6,10 +6,14 @@ b = float(input('Digite o valor da 2º reta: '))
 c = float(input('Digite o valor da 3º reta: '))
 print('\033[1;31;40m[Carregando...]\033[m')
 sleep(2)
-if (a==b) and (a==c):
-    print('EQUILATERO')
-elif (a==b) or (a==c) or (b==c):
-    print('ISOSCELES')
+if a < b + c and b < a + c and c < a + b:
+    print('As retas podem formar um triangulo ', end='')
+    if a == b == c:
+        print('Equilatero')
+    elif a != b != c != a:
+        print('Escaleno')
+    else:
+        print('Isosceles')
 else:
-    print('ESCALEANO')
+    print('As retas não podem formar um triangulo')
 print('xxxxxxxxxxxxxxxxxx FIM xxxxxxxxxxxxxxxxxx')
